5fdaafed521048994fdbeca9aec28d04042f176b
jessiewuwu <jwu004@gmail.com>
2015-11-28 23:38:08 -0800

    version 2 with effects and slider bannerfold

--- javascript_magic.js
+++ javascript_magic.js
@@ -1,1 +1,29 @@
-console.log('in it');
+var options = [
+  {selector: '#activities-list', offset: 300, callback: 'Materialize.showStaggeredList("#activities-list")' },
+];
+Materialize.scrollFire(options);
+
+
+
+$(document).ready(function(){
+  $('.slider').slider({
+  	full_width: true,
+  	interval: 1000,
+  	transition: 1000
+  });
+
+});
+
+
+
+
+// var auto;
+// if (current.id==1) { //this is the only slide I want lasting 10 seconds.
+//     auto=setTimeout(function(){
+//        next(); 
+//     },10*1000); //10 seconds for MAIN slide
+// } else { // all other slides should interval every 4 seconds.
+//     auto=setTimeout(function(){
+//        next();
+//     },4*1000);//4 seconds each other slide.
+// }
