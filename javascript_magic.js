59f7685111afcd44048878efb9fb0469d88ba721
jessiewuwu <jwu004@gmail.com>
2016-01-31 14:06:19 -0800

    Clean up comments and move body css

--- javascript_magic.js
+++ javascript_magic.js
@@ -17,16 +17,3 @@ $(document).ready(function(){
    Materialize.fadeInImage('.banner-img');
 });
 
-
-
-
-// var auto;
-// if (current.id==1) { //this is the only slide I want lasting 10 seconds.
-//     auto=setTimeout(function(){
-//        next(); 
-//     },10*1000); //10 seconds for MAIN slide
-// } else { // all other slides should interval every 4 seconds.
-//     auto=setTimeout(function(){
-//        next();
-//     },4*1000);//4 seconds each other slide.
-// }
