f1df5e20cecdf31f3a35b217d2b732dc7340110e
jessiewuwu <jwu004@gmail.com>
2016-01-31 13:53:41 -0800

    Add favicon and slow down the slider

--- javascript_magic.js
+++ javascript_magic.js
@@ -5,14 +5,16 @@ Materialize.scrollFire(options);
 
 
 
+
 $(document).ready(function(){
   $('.slider').slider({
   	full_width: true,
-  	interval: 1000,
-  	transition: 1000
+  	interval: 1500,
+  	transition: 1000,
+  	indicators: false
   });
    $(".button-collapse").sideNav();
-
+   Materialize.fadeInImage('.banner-img');
 });
 
 
